f1772c881b2b972d6733988f570a339f671d7562
Minh Van <minh.van1789@gmail.com>
2016-10-31 17:22:51 +0700

    Clean article

--- ngx-blog/articles/1477889733/examples/index.js
+++
@@ -1,9 +0,0 @@
-export {
-  reactiveFormsComponent,
-  reactiveFormsComponentModule
-} from './custom-validator-for-reactive-form';
-
-export {
-  templateDrivenFormsComponent,
-  templateDrivenFormsModule
-} from './custom-validator-for-template-driven-form';
